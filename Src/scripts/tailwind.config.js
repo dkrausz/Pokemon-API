9baf03d6d6eb82185656e0967ce9ed50bf8831fe
Danilo Krausz <danilo.krausz@gmail.com>
2023-11-16 15:18:17 +0100

    Começando a estilizacao com tailwind

---
+++ Src/scripts/tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        }
+      }
+    }
+  }
+
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'pokemon1': "url('./Src/assets/bg_1.jpg')",   
+          'pokemon2': "url('../assets/bg_2.webp')",           
+        }
+      }
+    }
+  }
+  
